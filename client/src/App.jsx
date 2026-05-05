a8795da4dabefdb7f384bf020f48ff629fef963d
Srushti-Jadhav03 <jadhavsrushti3804@gmail.com>
2026-05-05 11:49:13 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import Footer from './components/Footer';
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Ticker from './components/Ticker'
+import TradingAssets from './components/TradingAssets'
+import HowItWorks from './components/HowItWorks'
+import PricingSection from './components/PricingSection'
+import WhyUs from './components/WhyUs'
+import EarningsCalculator from './components/EarningsCalculator'
+import TopTraders from './components/TopTraders'
+import Testimonials from './components/Testimonials'
+import FAQ from './components/FAQ'
+import CTABanner from './components/CTABanner'
+import Footer from './components/Footer'
+import './App.css'
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
+    <div style={{ overflowX: 'hidden', width: '100%' }}>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <Hero />
+      <Ticker />
+      <TradingAssets />
+      <HowItWorks />
+      <PricingSection />
+      <WhyUs />
+      <EarningsCalculator />
+      <TopTraders />
+      <Testimonials />
+      <FAQ />
+      <CTABanner />
       <Footer />
-    </BrowserRouter>
-  );
+    </div>
+  )
 }
+export default App
